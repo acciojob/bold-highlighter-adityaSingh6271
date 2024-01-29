a022c674ddbc72b47e627be6dc67c0823b4f83f9
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-29 12:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// script.js
+
 function highlight() {
-    //Write your code here
+  // Get all <strong> elements in the second paragraph
+  const boldWords = document.querySelectorAll('p:nth-of-type(2) strong');
 
-
+  // Change the color of each bold word to green
+  boldWords.forEach(word => {
+    word.style.color = 'green';
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
+  // Get all <strong> elements in the second paragraph
+  const boldWords = document.querySelectorAll('p:nth-of-type(2) strong');
 
-    
+  // Revert the color of each bold word back to black
+  boldWords.forEach(word => {
+    word.style.color = 'black';
+  });
 }
